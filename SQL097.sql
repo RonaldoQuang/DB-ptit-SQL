25af4f29e4097647f4cc6ac8381db9ccaa3f93e5
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-11-30 16:26:34 +0700

    Update SQL097.sql

--- SQL097.sql
+++ SQL097.sql
@@ -2,5 +2,7 @@ select b1.machine_id, round(sum(b1.tinh)/count(b1.tinh),3) as processing_time
 from (select a1.machine_id, (a2.timestamp-a1.timestamp) as tinh
       from Activity as a1
       join Activity as a2
-      on a1.machine_id=a2.machine_id and a1.process_id=a2.process_id and a1.timestamp<a2.timestamp) as b1
+      on a1.machine_id=a2.machine_id 
+      and a1.process_id=a2.process_id 
+      and a1.timestamp<a2.timestamp) as b1
 group by b1.machine_id;
