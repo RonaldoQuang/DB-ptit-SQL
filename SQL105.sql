4e17c3c5b122e46f39a388730edc4450d79e654b
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-11-30 22:37:12 +0700

    Update SQL105.sql

--- SQL105.sql
+++ SQL105.sql
@@ -1,9 +1,9 @@
 select i.username, c.dept, c.number, c.title
 from instructor as i
 join teaches as t
-  on i.username = t.username
+on i.username = t.username
 join class as c
-  on t.dept = c.dept
-  and t.number = c.number
+on t.dept = c.dept
+and t.number = c.number
 order by i.lname
 limit 2;
