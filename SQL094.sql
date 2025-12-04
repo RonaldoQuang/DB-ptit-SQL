b78a0d3fb7925d8802663251169d4c9eedeacaf1
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-12-04 15:43:36 +0700

    Update SQL094.sql

--- SQL094.sql
+++ SQL094.sql
@@ -1,6 +1,7 @@
 select distinct l1.num as ConsecutiveNums
-from logs l1
-join logs l2 on l2.id = l1.id + 1
-join logs l3 on l3.id = l1.id + 2
-where l1.num = l2.num and l2.num = l3.num
-order by ConsecutiveNums;
+from Logs as l1
+join Logs as l2
+on l2.id=l1.id+1 and l1.num=l2.num
+join Logs as l3
+on l3.id=l2.id+1 and l2.num=l3.num
+order by l1.num;
