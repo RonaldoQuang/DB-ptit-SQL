086115701a4eedf80b1a5556fe0c6ea0e88e3e2f
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-12-25 23:27:38 +0700

    Add files via upload

---
+++ SQL2717.sql
@@ -0,0 +1,12 @@
+select p1.name as provider_name, sum(p2.power_generated) as total_power_generated
+from region as r1
+join provider as p1
+on r1.id=p1.region_id and r1.name='Midwest'
+join renewable_source as r2
+on p1.id=r2.provider_id and r2.name='Wind'
+join power_generation as p2
+on r2.id=p2.source_id 
+where p2.date between '2021-04-01' and '2021-06-30'
+group by p1.name
+order by total_power_generated desc
+limit 3;
